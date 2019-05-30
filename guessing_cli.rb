f9655955cadc8a4bd17e92d42bca21d340622006
Trevahr Robertson <trevahr@yahoo.com>
2019-05-30 02:43:06 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,53 @@
 # Code your solution here!
+def welcome
+  guess = ""
+  while (1..6).include?(guess) == false
+  puts "PLease enter a number between 1 - 6"
+  if guess == "exit"
+    puts "The computer guessed #{computer_guess}"
+    break
+  else
+  guess = gets.to_i
+end
+end
+ your_guess = guess
+end
+
+
+
+
+
+def random_number_generator
+    
+  computer_guess = rand(1..6)
+
+end
+
+
+
+
+def compare_numbers
+  
+  if your_guess == computer_guess
+    "You guessed the correct number!"
+  else
+     puts "The computer guessed #{computer_guess}"
+end
+end
+
+
+def run_guessing_game
+your_guess = ""
+computer_guess = ""
+random_number_generator
+welcome
+compare_numbers
+  
+
+end
+
+
+
+
+
+
